37c9a5cb2cf969983226f71624d2469c5d48d356
unknown <giokhabu@gamil.com>
2024-01-09 18:33:01 +0400

    feat: accomplish frist tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,11 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number < 0) {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -38,8 +41,14 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  }
+  if (b > a && b > c) {
+    return b;
+  }
+  return c;
 }
 
 /**
@@ -82,8 +91,14 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false;
+  }
+  if (a + b > c && a + c > b && c + b > a) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -100,8 +115,20 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let num2 = num;
+  const roman = ['X', 'IX', 'V', 'IV', 'I'];
+  const arabic = [10, 9, 5, 4, 1];
+  let index = 0;
+  let result = '';
+  while (num2 > 0) {
+    if (num2 >= arabic[index]) {
+      result += roman[index];
+      num2 -= arabic[index];
+    } else index += 1;
+  }
+
+  return result;
 }
 
 /**
@@ -119,8 +146,101 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let stringify = '';
+  for (let i = 0; i <= String(numberStr).length; i += 1) {
+    if (i === 0) {
+      switch (String(numberStr[i])) {
+        case '.':
+          stringify = `${stringify}point`;
+          break;
+        case ',':
+          stringify = `${stringify}point`;
+          break;
+        case '-':
+          stringify = `${stringify}minus`;
+          break;
+        case '0':
+          stringify = `${stringify}zero`;
+          break;
+        case '1':
+          stringify = `${stringify}one`;
+          break;
+        case '2':
+          stringify = `${stringify}two`;
+          break;
+        case '3':
+          stringify = `${stringify}three`;
+          break;
+        case '4':
+          stringify = `${stringify}four`;
+          break;
+        case '5':
+          stringify = `${stringify}five`;
+          break;
+        case '6':
+          stringify = `${stringify}six`;
+          break;
+        case '7':
+          stringify = `${stringify}seven`;
+          break;
+        case '8':
+          stringify = `${stringify}eight`;
+          break;
+        case '9':
+          stringify = `${stringify}nine`;
+          break;
+        default:
+          stringify = `${stringify}`;
+      }
+    } else {
+      switch (String(numberStr[i])) {
+        case '.':
+          stringify = `${stringify} point`;
+          break;
+        case ',':
+          stringify = `${stringify} point`;
+          break;
+        case '-':
+          stringify = `${stringify} minus`;
+          break;
+        case '0':
+          stringify = `${stringify} zero`;
+          break;
+        case '1':
+          stringify = `${stringify} one`;
+          break;
+        case '2':
+          stringify = `${stringify} two`;
+          break;
+        case '3':
+          stringify = `${stringify} three`;
+          break;
+        case '4':
+          stringify = `${stringify} four`;
+          break;
+        case '5':
+          stringify = `${stringify} five`;
+          break;
+        case '6':
+          stringify = `${stringify} six`;
+          break;
+        case '7':
+          stringify = `${stringify} seven`;
+          break;
+        case '8':
+          stringify = `${stringify} eight`;
+          break;
+        case '9':
+          stringify = `${stringify} nine`;
+          break;
+        default:
+          stringify = `${stringify}`;
+      }
+    }
+  }
+
+  return stringify;
 }
 
 /**
@@ -135,8 +255,14 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const len = str.length;
+  for (let i = 0; i < len / 2; i += 1) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
